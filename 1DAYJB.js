d77a1068262265cd514732343ac7a940c7423b9d
LostXofs <lostmmma@gmail.com>
2024-12-15 21:22:06 +0100

    Create 1DAYJB.js

---
+++ 1DAYJB.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "Unknown") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+window.alert(" Usa Jb First And Usa Dns ");
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ( 1 DAY )");
+
+ }
