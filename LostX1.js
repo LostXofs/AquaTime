ec8350d29884d4c645b673fb4b489d80250b4855
LostXofs <lostmmma@gmail.com>
2025-01-04 10:14:56 +0100

    Create LostX1.js

---
+++ LostX1.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "LostXsensi") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+window.alert(" 𝐀𝐜𝐭𝐢𝐯𝐞 𝐒𝐩𝐞𝐞𝐝 𝐋𝐚𝐬𝐭 𝐖𝐚𝐧 ");
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 𝗦𝗘𝗡𝗦𝗜 𝗨𝗦𝗘𝗥𝗦");
+
+ }
